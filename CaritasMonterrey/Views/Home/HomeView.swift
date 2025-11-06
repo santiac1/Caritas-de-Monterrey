0e52af3e6e277eb8aad1dc4c7c01b462050e59c4
victorvalero6 <a01383804@tec.mx>
2025-11-05 21:56:45 -0600

    sheet crear donacion pantalla completa

--- CaritasMonterrey/Views/Home/HomeView.swift
+++ CaritasMonterrey/Views/Home/HomeView.swift
@@ -102,7 +102,7 @@ struct HomeView: View {
 
             .sheet(isPresented: $showDonationSheet) {
                 DonationSheet(viewModel: DonationSheetViewModel())
-                    .presentationDetents([.medium, .large])
+                    .presentationDetents([.large])
             }
         }
     }
